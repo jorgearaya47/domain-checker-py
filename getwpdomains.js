6b5401859b45108be98e1b7f04f26bde1a669dd7
jorgearaya47 <jorge.araya@bvmedia.cr>
2023-06-02 11:56:37 -0600

    update js function

--- getwpdomains.js
+++ getwpdomains.js
@@ -4,7 +4,7 @@
  */
 
 function getWpSitesDomains() {
-    const tableDomains = document.querySelectorAll('td.blogname a.edit');
+    const tableDomains = document.querySelectorAll('td.blogname strong > a.edit');
     const domains = Array.from(tableDomains).map(domain => domain.innerText);
     return domains
 }
